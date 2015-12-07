307d388ed88fc1dd2c671683c42300127f7ae4a8
Alex Naidis <alex.naidis@linux.com>
2015-12-07 18:56:50 +0100

    config: common: use Candy identifier in any case

--- config/common.mk
+++ config/common.mk
@@ -33,13 +33,8 @@ PRODUCT_BOOTANIMATION := vendor/candy/prebuilt/common/bootanimation/$(TARGET_BOO
 endif
 endif
 
-ifdef CANDY_NIGHTLY
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.rommanager.developerid=candynightly
-else
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.rommanager.developerid=candy
-endif
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 
