f7a7a30934a4062baea104b7adb4c6a68ac51321
Raleigh Matlock <raleighman2@gmail.com>
2015-12-21 12:03:47 -0500

    Remove the CM SDK and CM overlay items from vendor

--- config/common.mk
+++ config/common.mk
@@ -135,15 +135,13 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     Launcher3 \
     AudioFX \
-    Eleven \
-    LockClock \
-    ExactCalculator
+    LockClock
 
-# Candy Platform Library
-PRODUCT_PACKAGES += \
-    org.cyanogenmod.platform-res \
-    org.cyanogenmod.platform \
-    org.cyanogenmod.platform.xml
+# CM Platform Library
+#PRODUCT_PACKAGES += \
+#    org.cyanogenmod.platform-res \
+#    org.cyanogenmod.platform \
+#    org.cyanogenmod.platform.xml
 
 # Candy Hardware Abstraction Framework
 PRODUCT_PACKAGES += \
@@ -312,33 +310,9 @@ endif
 # by default, do not update the recovery with system updates
 PRODUCT_PROPERTY_OVERRIDES += persist.sys.recovery_update=false
 
-ifndef CM_PLATFORM_SDK_VERSION
-  # This is the canonical definition of the SDK version, which defines
-  # the set of APIs and functionality available in the platform.  It
-  # is a single integer that increases monotonically as updates to
-  # the SDK are released.  It should only be incremented when the APIs for
-  # the new release are frozen (so that developers don't write apps against
-  # intermediate builds).
-  CM_PLATFORM_SDK_VERSION := 4
-endif
-
-ifndef CM_PLATFORM_REV
-  # For internal SDK revisions that are hotfixed/patched
-  # Reset after each CM_PLATFORM_SDK_VERSION release
-  # If you are doing a release and this is NOT 0, you are almost certainly doing it wrong
-  CM_PLATFORM_REV := 0
-endif
 
 PRODUCT_PROPERTY_OVERRIDES += \
   ro.candy.display.version=$(CANDY_DISPLAY_VERSION)
-
-# CyanogenMod Platform SDK Version
-PRODUCT_PROPERTY_OVERRIDES += \
-  ro.cm.build.version.plat.sdk=$(CM_PLATFORM_SDK_VERSION)
-
-# CyanogenMod Platform Internal
-PRODUCT_PROPERTY_OVERRIDES += \
-  ro.cm.build.version.plat.rev=$(CM_PLATFORM_REV)
 
 -include $(WORKSPACE)/build_env/image-auto-bits.mk
 
