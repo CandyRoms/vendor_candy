67eefa65f1a8a358047b6bdf483975679eade7f9
George G <kreach3r@users.noreply.github.com>
2015-12-26 14:51:50 +0200

    Layers: dynamic backup script
    Backup all overlay apks located inside /system/vendor/overlay
    dynamically, during dirty flash.
    
    No more hardcoding, Tasker plugins, external apps.
    
    A script by @KreAch3R
    
    (Full credit for finding this commit goes to DJ Klein...)
    
    Change-Id: Ia9d30771370cfb7bcb0b3d74653afd61213a82bc

--- config/common.mk
+++ config/common.mk
@@ -118,6 +118,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     vendor/candy/prebuilt/common/app/LayersManager/LayersManager.apk:system/app/LayersManager/LayersManager.apk
 
+# Layers Backup
+PRODUCT_COPY_FILES += \
+    vendor/candy/prebuilt/common/addon.d/71-layers.sh:system/addon.d/71-layers.sh
+
 # Hide BitSyko Layers Manager app icon from launcher
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.layers.noIcon=noIcon
