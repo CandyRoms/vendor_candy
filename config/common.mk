UBERROMS/vendor

Add back system squisher for nougat kudo to chet

config/common.mk
@@ -75,5 +75,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     Exchange2 \
     LockClock \
-    OmniSwitch \
     masquerade
+
+# Squisher Location
+SQUISHER_SCRIPT := vendor/uber/tools/squisher

CandyRoms/vendor_candy

Change-Id: Id5cc9d0c668350e5bf64505acfbecdc7cec8fa1c

config/common.mk
@@ -186,3 +186,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.candy.buildtype=$(CANDY_BUILD_TYPE)
 
 EXTENDED_POST_PROCESS_PROPS := vendor/candy/tools/candy_process_props.py
+
+# Squisher Location
+SQUISHER_SCRIPT := vendor/candy/tools/squisher