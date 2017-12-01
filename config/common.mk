5c2740748b3de67c5a54da33ec812437179390d2
Dan Cartier <nospamdan1@gmail.com>
2017-12-01 01:41:20 -0500

    DUI: Initial checkin for Oreo [2/7]
    Also remove deprecated cm charger stuff...
    
    Change-Id: Id8beae19dd47927e1459eaf83370c3c5674c57b6

--- config/common.mk
+++ config/common.mk
@@ -111,9 +111,46 @@ PRODUCT_PACKAGES += \
     ntfsfix \
     ntfs-3g
 
-
 PRODUCT_PACKAGES += \
     charger_res_images
+
+# MusicFX advanced effects
+#ifneq ($(TARGET_NO_DSPMANAGER), true)
+#PRODUCT_PACKAGES += \
+#    libcyanogen-dsp \
+#    audio_effects.conf
+#endif
+
+
+# DU Utils library
+PRODUCT_BOOT_JARS += \
+    org.dirtyunicorns.utils
+
+# DU Utils library
+PRODUCT_PACKAGES += \
+    org.dirtyunicorns.utils
+
+#ifeq ($(DEFAULT_ROOT_METHOD),magisk)
+# Magisk Manager
+#PRODUCT_PACKAGES += \
+#    MagiskManager
+
+# Magisk
+#PRODUCT_COPY_FILES += \
+#   vendor/candy/prebuilt/common/addon.d/magisk.zip:system/addon.d/magisk.zip
+#endif
+
+#ifeq ($(DEFAULT_ROOT_METHOD),supersu)
+# SuperSU
+#PRODUCT_COPY_FILES += \
+#   vendor/candy/prebuilt/common/etc/UPDATE-SuperSU.zip:system/addon.d/UPDATE-SuperSU.zip \
+#   vendor/candy/prebuilt/common/etc/init.d/99SuperSUDaemon:system/etc/init.d/99SuperSUDaemon
+#endif
+
+# Explict rootless defined, or none of the root methods defined,
+# default rootless : nothing todo
+#ifeq ($(DEFAULT_ROOT_METHOD),rootless)
+#endif
 
 # Stagefright FFMPEG plugin
 PRODUCT_PACKAGES += \
