0490406b72f21cc8ae4af61ed5524cfde742666c
Dan Cartier <nospamdan1@gmail.com>
2018-08-17 07:34:53 -0400

    candy: Update version numbers for Pie 9.0
    Change-Id: I7d5b5ce6ef17e7085e7b5f1eb40239b0bd55b37b

--- config/common.mk
+++ config/common.mk
@@ -184,7 +184,7 @@ PRODUCT_PACKAGE_OVERLAYS += vendor/candy/overlay/common
 
 # Versioning System
 # candy first version.
-PRODUCT_VERSION = 8.1b
+PRODUCT_VERSION = 9.0
 CANDY_POSTFIX := -$(shell date +"%Y%m%d-%H%M")
 
 ifdef CANDY_BUILD_EXTRA
