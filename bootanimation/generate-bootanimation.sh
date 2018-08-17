SlimRoms/vendor_slim

Update bootanimation
  * Taken from Lineage

Change-Id: Icd905d43f912bb5b1e8ee2a535c1b98eb0bd4ee2

bootanimation/generate-bootanimation.sh
@@ -6,32 +6,23 @@ HEIGHT=$(echo "$WIDTH/1.5" | bc)
 RWIDTH=$WIDTH
 RHEIGHT=$HEIGHT
 HALF_RES=$2
+OUT="$ANDROID_PRODUCT_OUT/obj/BOOTANIMATION"
+
 if [ "$HALF_RES" = "true" ]; then
     WIDTH=`expr $WIDTH / 2`
     HEIGHT=`expr $HEIGHT / 2`
 fi
-
-if [ -f "/usr/bin/convert" ]; then
-if [ -f "$ANDROID_PRODUCT_OUT/system/media/bootanimation.zip" ]; then
-    echo "$ANDROID_PRODUCT_OUT/system/media/bootanimation.zip"
-else
 RESOLUTION=""$WIDTH"x"$HEIGHT""
 
 mkdir -p $ANDROID_PRODUCT_OUT/obj/BOOTANIMATION/bootanimation/part{0..4}
-tar xvfp "$PWD/vendor/slim/bootanimation/bootanimation.tar" --to-command="convert - -resize '$RESOLUTION' \"png8:$ANDROID_PRODUCT_OUT/obj/BOOTANIMATION/bootanimation/\$TAR_FILENAME\""
+
+tar xfp "$PWD/vendor/slim/bootanimation/bootanimation.tar" -C "$OUT/bootanimation/"
+mogrify -resize $RESOLUTION -colors 250 "$OUT/bootanimation/"*"/"*".png"
+
 # create desc.txt
 echo "$RWIDTH" "$RHEIGHT" 60 > "$ANDROID_PRODUCT_OUT/obj/BOOTANIMATION/bootanimation/desc.txt"
 cat "$PWD/vendor/slim/bootanimation/desc.txt" >> "$ANDROID_PRODUCT_OUT/obj/BOOTANIMATION/bootanimation/desc.txt"
 
 # create bootanimation.zip
-cd "$ANDROID_PRODUCT_OUT/obj/BOOTANIMATION/bootanimation"
-
-if [ ! -d "$ANDROID_PRODUCT_OUT/system/media" ]; then
-mkdir -p "$ANDROID_PRODUCT_OUT/system/media"
-fi
-
-zip -r0 "$ANDROID_PRODUCT_OUT/system/media/bootanimation.zip" .
-echo "$ANDROID_PRODUCT_OUT/system/media/bootanimation.zip"
-
-fi
-fi
+cd "$OUT/bootanimation"
+zip -r0 "$ANDROID_PRODUCT_OUT/obj/BOOTANIMATION/bootanimation.zip" .

CandyRoms/vendor_candy

bootanimation/generate-bootanimation.sh
@@ -5,33 +5,24 @@ WIDTH=$1
 HEIGHT=$2
 RWIDTH=$WIDTH
 RHEIGHT=$HEIGHT
-HALF_RES=$3
+HALF_RES=$2
+OUT="$ANDROID_PRODUCT_OUT/obj/BOOTANIMATION"
+
 if [ "$HALF_RES" = "true" ]; then
     WIDTH=`expr $WIDTH / 2`
     HEIGHT=`expr $HEIGHT / 2`
 fi
-
-if [ -f "/usr/bin/convert" ]; then
-if [ -f "$ANDROID_PRODUCT_OUT/system/media/bootanimation.zip" ]; then
-    echo "$ANDROID_PRODUCT_OUT/system/media/bootanimation.zip"
-else
 RESOLUTION=""$WIDTH"x"$HEIGHT""
 
 mkdir -p $ANDROID_PRODUCT_OUT/obj/BOOTANIMATION/bootanimation/part{0..3}
-tar xvfp "$PWD/vendor/candy/bootanimation/bootanimation.tar" --to-command="convert - -resize '$RESOLUTION' \"png8:$ANDROID_PRODUCT_OUT/obj/BOOTANIMATION/bootanimation/\$TAR_FILENAME\""
+
+tar xfp "$PWD/vendor/candy/bootanimation/bootanimation.tar" -C "$OUT/bootanimation/"
+mogrify -resize $RESOLUTION -colors 250 "$OUT/bootanimation/"*"/"*".png"
+
 # create desc.txt
 echo "$RWIDTH" "$RHEIGHT" 30 > "$ANDROID_PRODUCT_OUT/obj/BOOTANIMATION/bootanimation/desc.txt"
 cat "$PWD/vendor/candy/bootanimation/desc.txt" >> "$ANDROID_PRODUCT_OUT/obj/BOOTANIMATION/bootanimation/desc.txt"
 
 # create bootanimation.zip
-cd "$ANDROID_PRODUCT_OUT/obj/BOOTANIMATION/bootanimation"
-
-if [ ! -d "$ANDROID_PRODUCT_OUT/system/media" ]; then
-mkdir -p "$ANDROID_PRODUCT_OUT/system/media"
-fi
-
-zip -r0 "$ANDROID_PRODUCT_OUT/system/media/bootanimation.zip" .
-echo "$ANDROID_PRODUCT_OUT/system/media/bootanimation.zip"
-
-fi
-fi
+cd "$OUT/bootanimation"
+zip -r0 "$ANDROID_PRODUCT_OUT/obj/BOOTANIMATION/bootanimation.zip" .