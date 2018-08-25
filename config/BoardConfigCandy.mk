499fc5a89d44dd6b7bec2a125708d02ab6e3fcd0
Rashed Abdel-Tawab <rashed@linux.com>
2018-08-25 11:10:25 -0700

    candy: Move QC board variables earlier
    These variables are usually set in a device's BoardConfig, setting them
    in qcom_target is at the very end of the "configuration process" which
    results in them being unavailable to plenty of other configuration
    "things" (ex. soong namespaces or soong config). Move them to right
    after a device's BoardConfig has been found and loaded.
    
    Change-Id: Iddd731202d22ed3f8eb010197ce20d3c75a1f40a

--- config/BoardConfigCandy.mk
+++ config/BoardConfigCandy.mk
@@ -1,4 +1,8 @@
 # Charger
-#ifneq ($(WITH_CANDY_CHARGER),false)
-#    BOARD_HAL_STATIC_LIBRARIES := libhealthd.candy
+#ifeq ($(WITH_LINEAGE_CHARGER),true)
+#    BOARD_HAL_STATIC_LIBRARIES := libhealthd.lineage
 #endif
+
+ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
+include vendor/candy/config/BoardConfigQcom.mk
+endif
