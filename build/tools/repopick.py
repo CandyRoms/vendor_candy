383e3387f1451c632e7d458e0f55a7e75e4edf7f
Adrian DC <radian.dc@gmail.com>
2018-08-30 23:07:23 +0200

    repopick: Warn about empty commits instead of failing
     * If a cherry-pick results in an empty commit
        (hints about git commit --allow-empty),
        we should warn instead of failing all repopicks
    
    Change-Id: I8410d7d02c4118c8072de609cf3c291e2d8523e6

--- build/tools/repopick.py
+++ build/tools/repopick.py
@@ -381,7 +381,19 @@ if __name__ == '__main__':
                 cmd_out = None
             result = subprocess.call(cmd, cwd=project_path, shell=True, stdout=cmd_out, stderr=cmd_out)
             if result != 0:
-                print('ERROR: git command failed')
-                sys.exit(result)
+                cmd = ['git diff-index --quiet HEAD --']
+                result = subprocess.call(cmd, cwd=project_path, shell=True, stdout=cmd_out, stderr=cmd_out)
+                if result == 0:
+                    print('WARNING: git command resulted with an empty commit, aborting cherry-pick')
+                    cmd = ['git cherry-pick --abort']
+                    subprocess.call(cmd, cwd=project_path, shell=True, stdout=cmd_out, stderr=cmd_out)
+                elif args.reset:
+                    print('ERROR: git command failed, aborting cherry-pick')
+                    cmd = ['git cherry-pick --abort']
+                    subprocess.call(cmd, cwd=project_path, shell=True, stdout=cmd_out, stderr=cmd_out)
+                    sys.exit(result)
+                else:
+                    print('ERROR: git command failed')
+                    sys.exit(result)
         if not args.quiet:
             print('')
