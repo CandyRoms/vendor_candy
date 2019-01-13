AOSiP/platform_vendor_aosip

Add default permissions for WeatherClient
Change-Id: Icba08d4b98452b9d57c771e1719fb002c7019cf5
Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

config/common.mk
@@ -69,6 +69,11 @@ $(foreach f,$(wildcard vendor/aosip/prebuilt/common/etc/init/*.rc),\
 PRODUCT_COPY_FILES +=  \
     vendor/aosip/prebuilt/common/sysconfig/dialer_experience.xml:system/etc/sysconfig/dialer_experience.xml
 
+# Weather client
+PRODUCT_COPY_FILES += \
+    vendor/aosip/prebuilt/common/etc/permissions/org.pixelexperience.weather.client.xml:system/etc/permissions/org.pixelexperience.weather.client.xml \
+    vendor/aosip/prebuilt/common/etc/default-permissions/org.pixelexperience.weather.client.xml:system/etc/default-permissions/org.pixelexperience.weather.client.xml
+
 # Storage manager
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.storage_manager.enabled=true

CandyRoms/vendor_candy

config/common.mk
@@ -114,6 +114,11 @@ PRODUCT_PACKAGES += \
     LiveWallpapersPicker \
     PhaseBeam
 
+# Weather client
+PRODUCT_COPY_FILES += \
+    vendor/candy/prebuilt/common/etc/permissions/org.pixelexperience.weather.client.xml:system/etc/permissions/org.pixelexperience.weather.client.xml \
+    vendor/candy/prebuilt/common/etc/default-permissions/org.pixelexperience.weather.client.xml:system/etc/default-permissions/org.pixelexperience.weather.client.xml
+
 # Prebuilt packages
 PRODUCT_PACKAGES += \
     nexuslauncher \