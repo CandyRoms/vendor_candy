1e63a333e1893d011a62da974cf0029cbf0d085a
Paul Keith <javelinanddart@gmail.com>
2019-02-19 17:52:53 +0100

    Revert "vendor/candy: Add support for java source overlays"
    This reverts commit af9697550607a5e2d206384b322fe65fe285e85c.
    
    Change-Id: I837a5a8e8523e9e9959abe83dabcc333e87eac17

--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -13,7 +13,6 @@ $(call add_json_bool, Device_support_hwfde, $(filter true,$(TARGET_HW_DISK_ENCRY
 $(call add_json_bool, Device_support_hwfde_perf, $(filter true,$(TARGET_HW_DISK_ENCRYPTION_PERF)))
 $(call add_json_bool, Device_support_legacy_hwfde, $(filter true,$(TARGET_LEGACY_HW_DISK_ENCRYPTION)))
 $(call add_json_bool, Device_support_wait_for_qsee, $(filter true,$(TARGET_KEYMASTER_WAIT_FOR_QSEE)))
-$(call add_json_str, Java_Source_Overlays, $(JAVA_SOURCE_OVERLAYS))
 $(call add_json_val_default, Bootloader_message_offset, $(BOOTLOADER_MESSAGE_OFFSET), 0)
 $(call add_json_str, Specific_camera_parameter_library, $(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY))
 $(call add_json_bool, Supports_hw_fde, $(filter true,$(TARGET_HW_DISK_ENCRYPTION)))
