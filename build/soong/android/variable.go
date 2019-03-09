10569a4acf38ab57d56695ef42854136f813f6b6
Erfan Abdi <erfangplus@gmail.com>
2019-03-09 12:03:41 +0200

    soong_config: Add flag for devices use metadata as FDE key
    Change-Id: I5f70280e72613a8f850cb3263c45e95e80cb8b3c
    Signed-off-by: Erfan Abdi <erfangplus@gmail.com>

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -41,6 +41,9 @@ type Product_variables struct {
     Uses_generic_camera_parameter_library struct {
         Srcs []string
     }
+	Uses_metadata_as_fde_key struct {
+		Cflags []string
+	}
 	Uses_qti_camera_device struct {
 		Cppflags []string
 		Shared_libs []string
@@ -93,6 +96,7 @@ type ProductVariables struct {
     Uses_generic_camera_parameter_library  *bool `json:",omitempty"`
     Uses_nvidia_enhancements  *bool `json:",omitempty"`
     Uses_qcom_bsp_legacy  *bool `json:",omitempty"`
+	Uses_metadata_as_fde_key  *bool `json:",omitempty"`
 	Uses_qti_camera_device  *bool `json:",omitempty"`
 	Uses_qcom_um_family  *bool `json:",omitempty"`
 	Uses_qcom_um_3_18_family  *bool `json:",omitempty"`
