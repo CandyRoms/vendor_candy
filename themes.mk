37b56c77e40a2305d50a0de2f5898e6e27a60b01
AnkitGourav <Ankitgourav035@gmail.com>
2019-03-23 15:50:03 +0530

    Add more styles to Switch Styler
    Change-Id: I48bf79d4a04dd11907a75a98d5abe48f877d9c3a

---
+++ themes.mk
@@ -0,0 +1,54 @@
+# Fruity Pebbles 2/2
+PRODUCT_PACKAGES += \
+    UserOne \
+    UserTwo \
+    UserThree \
+    UserFour \
+    UserFive \
+    UserSix \
+    UserSeven
+
+# Dark themes
+PRODUCT_PACKAGES += \
+    SettingsDarkTheme \
+    SystemDarkTheme \
+    SysuiQsDarkTheme \
+    GBoardDarkTheme \
+    WellbeingDarkTheme \
+    GoogleIntelligenceSenseDarkTheme
+
+# Black themes
+PRODUCT_PACKAGES += \
+    SettingsBlackTheme \
+    SystemBlackTheme \
+    SysuiQsBlackTheme \
+    GBoardBlackTheme \
+    WellbeingBlackTheme \
+    GoogleIntelligenceSenseBlackTheme
+
+# Light themes
+PRODUCT_PACKAGES += \
+    GoogleIntelligenceSenseLightTheme \
+    GBoardLightTheme
+
+# QS tile styles
+PRODUCT_PACKAGES += \
+    QStileCircleTrim \
+    QStileDualToneCircle \
+    QStileHexicon \
+    QStileSquircleTrim
+
+# Switch styler
+PRODUCT_PACKAGES += \
+    MD2Switch \
+    OnePlusSwitch \
+    StockSwitch \
+    SwitchOne \
+    SwitchTwo \
+    SwitchThree \
+    SwitchFour \
+    SwitchFive \
+    SwitchSix \
+    SwitchSeven \
+    SwitchEight \
+    SwitchNine
