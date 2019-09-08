1abf218f4c71ceaa5bb1533615bb203920b505c6
Rashed Abdel-Tawab <rashed@linux.com>
2019-09-08 00:27:57 -0700

    qcom: Set a pathmap variable for qcom superproject
    This allows us to define the root dir for each platform, so we can
    guard the makefiles in a makefile in each platform root
    
    Change-Id: If4bb55637de3e734e7da9494fd4a3eaf7619a461

--- build/core/qcom_target.mk
+++ build/core/qcom_target.mk
@@ -17,6 +17,8 @@ endef
 
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
 
+$(call set-device-specific-path,PLATFORM_SUPERPROJECT,platform-superproject,hardware/qcom-caf/$(QCOM_HARDWARE_VARIANT))
+
 $(call set-device-specific-path,AUDIO,audio,hardware/qcom-caf/$(QCOM_HARDWARE_VARIANT)/audio)
 $(call set-device-specific-path,DISPLAY,display,hardware/qcom-caf/$(QCOM_HARDWARE_VARIANT)/display)
 $(call set-device-specific-path,MEDIA,media,hardware/qcom-caf/$(QCOM_HARDWARE_VARIANT)/media)
@@ -38,16 +40,18 @@ PRODUCT_CFI_INCLUDE_PATHS += \
     hardware/qcom-caf/wlan/qcwcn/wpa_supplicant_8_lib
 else
 
-    $(call project-set-path,qcom-audio,hardware/qcom/audio/default)
-    $(call project-set-path,qcom-display,hardware/qcom/display/$(TARGET_BOARD_PLATFORM))
-    $(call project-set-path,qcom-media,hardware/qcom/media/$(TARGET_BOARD_PLATFORM))
+$(call set-device-specific-path,PLATFORM_SUPERPROJECT,platform-superproject,hardware/qcom)
 
-    $(call project-set-path,qcom-camera,hardware/qcom/camera)
-    $(call project-set-path,qcom-data-ipa-cfg-mgr,hardware/qcom/data/ipacfg-mgr)
-    $(call project-set-path,qcom-gps,hardware/qcom/gps)
-    $(call project-set-path,qcom-sensors,hardware/qcom/sensors)
-    $(call project-set-path,qcom-loc-api,vendor/qcom/opensource/location)
-    $(call project-set-path,qcom-dataservices,$(TARGET_DEVICE_DIR)/dataservices)
+$(call project-set-path,qcom-audio,hardware/qcom/audio/default)
+$(call project-set-path,qcom-display,hardware/qcom/display/$(TARGET_BOARD_PLATFORM))
+$(call project-set-path,qcom-media,hardware/qcom/media/$(TARGET_BOARD_PLATFORM))
+
+$(call project-set-path,qcom-camera,hardware/qcom/camera)
+$(call project-set-path,qcom-data-ipa-cfg-mgr,hardware/qcom/data/ipacfg-mgr)
+$(call project-set-path,qcom-gps,hardware/qcom/gps)
+$(call project-set-path,qcom-sensors,hardware/qcom/sensors)
+$(call project-set-path,qcom-loc-api,vendor/qcom/opensource/location)
+$(call project-set-path,qcom-dataservices,$(TARGET_DEVICE_DIR)/dataservices)
 
 $(call wlan-set-path-variant,qcom)
 $(call bt-vendor-set-path-variant,qcom)
