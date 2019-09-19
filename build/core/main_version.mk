9ca0a3c36ed6d4617e8ba929c809b3dd12562d18
Dan Cartier <nospamdan1@gmail.com>
2019-09-19 09:00:20 -0400

    vendor/candy: Rearrange versioning variables

--- build/core/main_version.mk
+++ build/core/main_version.mk
@@ -12,6 +12,34 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# This file is -included from vendor/candy/build/core/main.mk
+
+# Versioning System
+# candy first version.
+PRODUCT_VERSION_MAJOR = 10
+PRODUCT_VERSION_MINOR = 0
+PRODUCT_VERSION_MAINTENANCE = 0
+CANDY_DATE := $(shell date +"%Y%m%d-%H%M")
+
+ifdef CANDY_BUILD_EXTRA
+    CANDY_POSTFIX += $(CANDY_BUILD_EXTRA)
+endif
+
+# Set the default version to unofficial
+ifndef CANDY_BUILD_TYPE
+    CANDY_BUILD_TYPE := UNOFFICIAL
+endif
+
+# Set all versions
+CANDY_BUILD_VERSION := $(PRODUCT_VERSION_MAJOR)
+CANDY_VERSION := Candy-$(CANDY_BUILD)-$(CANDY_BUILD_VERSION)-$(CANDY_BUILD_TYPE)-$(CANDY_DATE)
+CANDY_MOD_VERSION := Candy-$(CANDY_BUILD)-$(CANDY_BUILD_VERSION)-$(CANDY_BUILD_TYPE)-$(CANDY_DATE)
+
+PRODUCT_VERSION := $(TARGET_PRODUCT)
+PRODUCT_VERSION := $(subst candy_,,$(PRODUCT_VERSION))
+
+ROM_FINGERPRINT := Candy/$(PLATFORM_VERSION)/$(PRODUCT_VERSION)/$(shell date -u +%H%M)
+
 ADDITIONAL_BUILD_PROPERTIES += \
     ro.candy.build.version=$(CANDY_BUILD_VERSION) \
     ro.candy.build.date=$(CANDY_DATE) \
--- config/common.mk
+++ config/common.mk
@@ -162,32 +162,6 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES  += \
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES  += \
     ro.storage_manager.enabled=true
 
-# Versioning System
-# candy first version.
-PRODUCT_VERSION_MAJOR = 10
-PRODUCT_VERSION_MINOR = 0
-PRODUCT_VERSION_MAINTENANCE = 0
-CANDY_DATE := $(shell date +"%Y%m%d-%H%M")
-
-ifdef CANDY_BUILD_EXTRA
-    CANDY_POSTFIX += $(CANDY_BUILD_EXTRA)
-endif
-
-# Set the default version to unofficial
-ifndef CANDY_BUILD_TYPE
-    CANDY_BUILD_TYPE := UNOFFICIAL
-endif
-
-# Set all versions
-CANDY_BUILD_VERSION := $(PRODUCT_VERSION_MAJOR)
-CANDY_VERSION := Candy-$(CANDY_BUILD)-$(CANDY_BUILD_VERSION)-$(CANDY_BUILD_TYPE)-$(CANDY_DATE)
-CANDY_MOD_VERSION := Candy-$(CANDY_BUILD)-$(CANDY_BUILD_VERSION)-$(CANDY_BUILD_TYPE)-$(CANDY_DATE)
-
-PRODUCT_VERSION := $(TARGET_PRODUCT)
-PRODUCT_VERSION := $(subst candy_,,$(PRODUCT_VERSION))
-
-ROM_FINGERPRINT := Candy/$(PLATFORM_VERSION)/$(PRODUCT_VERSION)/$(shell date -u +%H%M)
-
 # easy way to extend to add more packages
 -include vendor/extra/product.mk
 
