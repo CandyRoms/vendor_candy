5b037738cf99b8e4c5f660e2b8d7d2f3390814e9
Dan Cartier <nospamdan1@gmail.com>
2019-09-20 12:01:34 -0400

    config: Fixup naming issue

--- config/BoardConfigCandy.mk
+++ config/BoardConfigCandy.mk
@@ -1,12 +1,2 @@
-# Charger
-#ifeq ($(WITH_LINEAGE_CHARGER),true)
-#    BOARD_HAL_STATIC_LIBRARIES := libhealthd.lineage
-#endif
 include vendor/candy/config/BoardConfigKernel.mk
-
-ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
-include vendor/candy/config/BoardConfigQcom.mk
-endif
-
-
 include vendor/candy/config/BoardConfigSoong.mk
