59390925fe248f7ecb5849d83247053f158ac9d7
Dan Cartier <nospamdan1@gmail.com>
2019-09-20 00:38:03 -0400

    soong_config: Add missing JAVA_SOURCE_OVERLAYS

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -57,6 +57,7 @@ type ProductVariables struct {
 	Device_support_hwfde_perf  *bool `json:",omitempty"`
 	Device_support_legacy_hwfde  *bool `json:",omitempty"`
 	Device_support_wait_for_qsee  *bool `json:",omitempty"`
+	Java_Source_Overlays *string `json:",omitempty"`
 	Has_legacy_camera_hal1  *bool `json:",omitempty"`
 	Uses_media_extensions   *bool `json:",omitempty"`
 	Needs_text_relocations  *bool `json:",omitempty"`
