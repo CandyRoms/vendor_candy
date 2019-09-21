70a1cc55cb2672f36803c7c01afffbfd78d10140
Dan Cartier <nospamdan1@gmail.com>
2019-09-21 12:32:28 -0400

    candy: Fixup soong vars and remove mismerged BoardConfig

--- config/BoardConfigPotato.mk
+++
@@ -1,7 +0,0 @@
-include vendor/potato/config/BoardConfigKernel.mk
-
-ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
-include vendor/potato/config/BoardConfigQcom.mk
-endif
-
-include vendor/potato/config/BoardConfigSoong.mk
