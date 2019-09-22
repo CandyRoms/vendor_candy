2ff98c9640e7f1a328c76ccb9ef93d0b8014a812
Michael Bestas <mkbestas@lineageos.org>
2019-09-22 17:19:49 +0300

    Revert "soong_config: Add flag for devices use metadata as FDE key"
    * Qcom reverted the relevant commit in cryptfs_hw
    
    This reverts commit a4d6ce67e76769bea45a1538c3e2d52b12dd9a38.
    
    Change-Id: Ife1812ebc80946953e3d1a558617c94ddbc7fad4

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -1,112 +1,116 @@
 package android
 type Product_variables struct {
-	Additional_gralloc_10_usage_bits struct {
-		Cppflags []string
-	}
-	Should_skip_waiting_for_qsee struct {
-		Cflags []string
-	}
-	Device_support_hwfde struct {
-		Cflags []string
-		Header_libs []string
-		Shared_libs []string
-	}
-	Device_support_hwfde_perf struct {
-		Cflags []string
-	}
-	Device_support_legacy_hwfde struct {
-		Cflags []string
-	}
-	Device_support_wait_for_qsee struct {
-		Cflags []string
-	}
-	Bootloader_message_offset struct {
-		Cflags []string
-	}
-	Has_legacy_camera_hal1 struct {
-		Cflags []string
-	}
-	Uses_media_extensions struct {
-		Cflags []string
-	}
-	Needs_text_relocations struct {
-		Cppflags []string
-	}
-	Supports_hw_fde struct {
-		Cflags []string
-		Header_libs []string
-		Shared_libs []string
-	}
-	Supports_hw_fde_perf struct {
-		Cflags []string
-	}
-	Target_init_vendor_lib struct {
-		Whole_static_libs []string
-	}
-	Target_process_sdk_version_override struct {
-		Cppflags []string
-	}
-	Target_shim_libs struct {
-		Cppflags []string
-	}
-	Uses_generic_camera_parameter_library struct {
-		Srcs []string
-	}
-	Uses_qcom_bsp_legacy struct {
-		Cflags []string
-		Cppflags []string
-	}
-	Uses_qti_camera_device struct {
-		Cppflags []string
-		Shared_libs []string
-	}
-	Uses_qcom_um_family struct {
-		Cflags []string
-		Srcs []string
-	}
-	Uses_qcom_um_3_18_family struct {
-		Header_libs []string
-		Shared_libs []string
-	}
-	Uses_qcom_um_4_4_family struct {
-		Header_libs []string
-		Shared_libs []string
-	}
-	Uses_qcom_um_4_9_family struct {
-		Header_libs []string
-		Shared_libs []string
-	}
-	Uses_qcom_um_4_14_family struct {
-		Header_libs []string
-		Shared_libs []string
-	}
+    Additional_gralloc_10_usage_bits struct {
+        Cppflags []string
+    }
+    Should_skip_waiting_for_qsee struct {
+        Cflags []string
+    }
+    Device_support_hwfde struct {
+        Cflags []string
+        Header_libs []string
+        Shared_libs []string
+    }
+    Device_support_hwfde_perf struct {
+        Cflags []string
+    }
+    Device_support_legacy_hwfde struct {
+        Cflags []string
+    }
+    Device_support_wait_for_qsee struct {
+        Cflags []string
+    }
+    Bootloader_message_offset struct {
+        Cflags []string
+    }
+    Has_legacy_camera_hal1 struct {
+        Cflags []string
+    }
+    Uses_media_extensions struct {
+        Cflags []string
+    }
+    Needs_text_relocations struct {
+        Cppflags []string
+    }
+    Supports_hw_fde struct {
+        Cflags []string
+        Header_libs []string
+        Shared_libs []string
+    }
+    Supports_hw_fde_perf struct {
+        Cflags []string
+    }
+    Target_init_vendor_lib struct {
+        Whole_static_libs []string
+    }
+    Target_process_sdk_version_override struct {
+        Cppflags []string
+    }
+    Target_shim_libs struct {
+        Cppflags []string
+    }
+    Uses_generic_camera_parameter_library struct {
+        Srcs []string
+    }
+    Uses_nvidia_enhancements struct {
+        Cppflags []string
+    }
+    Uses_qcom_bsp_legacy struct {
+        Cflags []string
+        Cppflags []string
+    }
+    Uses_qti_camera_device struct {
+        Cppflags []string
+        Shared_libs []string
+    }
+    Uses_qcom_um_family struct {
+        Cflags []string
+        Srcs []string
+    }
+    Uses_qcom_um_3_18_family struct {
+        Header_libs []string
+        Shared_libs []string
+    }
+    Uses_qcom_um_4_4_family struct {
+        Header_libs []string
+        Shared_libs []string
+    }
+    Uses_qcom_um_4_9_family struct {
+        Header_libs []string
+        Shared_libs []string
+    }
+    Uses_qcom_um_4_14_family struct {
+        Header_libs []string
+        Shared_libs []string
+    }
 }
 
 type ProductVariables struct {
-	Additional_gralloc_10_usage_bits  *string `json:",omitempty"`
-	Bootloader_message_offset  *int `json:",omitempty"`
-	Device_support_hwfde  *bool `json:",omitempty"`
-	Device_support_hwfde_perf  *bool `json:",omitempty"`
-	Device_support_legacy_hwfde  *bool `json:",omitempty"`
-	Device_support_wait_for_qsee  *bool `json:",omitempty"`
-	Java_Source_Overlays *string `json:",omitempty"`
-	Has_legacy_camera_hal1  *bool `json:",omitempty"`
-	Uses_media_extensions   *bool `json:",omitempty"`
-	Needs_text_relocations  *bool `json:",omitempty"`
-	Target_specific_headers_include_dir  *string `json:",omitempty"`
-	Should_skip_waiting_for_qsee  *bool `json:",omitempty"`
-	Specific_camera_parameter_library  *string `json:",omitempty"`
-	Supports_hw_fde  *bool `json:",omitempty"`
-	Supports_hw_fde_perf  *bool `json:",omitempty"`
-	Target_process_sdk_version_override *string `json:",omitempty"`
-	Target_init_vendor_lib  *string `json:",omitempty"`
-	Target_shim_libs  *string `json:",omitempty"`
-	Uses_generic_camera_parameter_library  *bool `json:",omitempty"`
-	Uses_qcom_bsp_legacy  *bool `json:",omitempty"`
-	Uses_qti_camera_device  *bool `json:",omitempty"`
-	Uses_qcom_um_family  *bool `json:",omitempty"`
-	Uses_qcom_um_3_18_family  *bool `json:",omitempty"`
-	Uses_qcom_um_4_4_family  *bool `json:",omitempty"`
-	Uses_qcom_um_4_9_family  *bool `json:",omitempty"`
-	Uses_qcom_um_4_14_family  *bool `json:",omitempty"`
+    Additional_gralloc_10_usage_bits  *string `json:",omitempty"`
+    Bootloader_message_offset  *int `json:",omitempty"`
+    Device_support_hwfde  *bool `json:",omitempty"`
+    Device_support_hwfde_perf  *bool `json:",omitempty"`
+    Device_support_legacy_hwfde  *bool `json:",omitempty"`
+    Device_support_wait_for_qsee  *bool `json:",omitempty"`
+    Java_Source_Overlays *string `json:",omitempty"`
+    Has_legacy_camera_hal1  *bool `json:",omitempty"`
+    Uses_media_extensions   *bool `json:",omitempty"`
+    Needs_text_relocations  *bool `json:",omitempty"`
+    Target_specific_headers_include_dir  *string `json:",omitempty"`
+    Should_skip_waiting_for_qsee  *bool `json:",omitempty"`
+    Specific_camera_parameter_library  *string `json:",omitempty"`
+    Supports_hw_fde  *bool `json:",omitempty"`
+    Supports_hw_fde_perf  *bool `json:",omitempty"`
+    Target_process_sdk_version_override *string `json:",omitempty"`
+    Target_init_vendor_lib  *string `json:",omitempty"`
+    Target_shim_libs  *string `json:",omitempty"`
+    Uses_generic_camera_parameter_library  *bool `json:",omitempty"`
+    Uses_nvidia_enhancements  *bool `json:",omitempty"`
+    Uses_qcom_bsp_legacy  *bool `json:",omitempty"`
+    Uses_qti_camera_device  *bool `json:",omitempty"`
+    Uses_qcom_um_family  *bool `json:",omitempty"`
+    Uses_qcom_um_3_18_family  *bool `json:",omitempty"`
+    Uses_qcom_um_4_4_family  *bool `json:",omitempty"`
+    Uses_qcom_um_4_9_family  *bool `json:",omitempty"`
+    Uses_qcom_um_4_14_family  *bool `json:",omitempty"`
 }
