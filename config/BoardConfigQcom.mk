62385476d44b7e03b398c9662132864197ea41f8
NoSpamDan <nospamdan1@gmail.com>
2019-09-29 06:19:28 -0400

    config: Clean up BoardConfigQcom

--- config/BoardConfigQcom.mk
+++ config/BoardConfigQcom.mk
@@ -86,22 +86,12 @@ else
     MSM_VIDC_TARGET_LIST := $(TARGET_BOARD_PLATFORM)
     QCOM_HARDWARE_VARIANT := $(TARGET_BOARD_PLATFORM)
 endif
-endif
-endif
-endif
-endif
-endif
-endif
 
 PRODUCT_SOONG_NAMESPACES += \
     hardware/qcom-caf/$(QCOM_HARDWARE_VARIANT)
-
-PRODUCT_SOONG_NAMESPACES += \
-    hardware/qcom/audio-caf/$(QCOM_HARDWARE_VARIANT) \
-    hardware/qcom/display-caf/$(QCOM_HARDWARE_VARIANT) \
-    hardware/qcom/media-caf/$(QCOM_HARDWARE_VARIANT)
 
 # QCOM HW crypto
 ifeq ($(TARGET_HW_DISK_ENCRYPTION),true)
     TARGET_CRYPTFS_HW_PATH ?= vendor/qcom/opensource/cryptfs_hw
 endif
+
