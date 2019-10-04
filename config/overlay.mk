fe8ce89fc7cbc0a8f2c2e9308874823b229dd6c8
Anay Wadhera <anay1018@gmail.com>
2019-10-04 11:26:01 -0700

    accent: drop unused overlays
    Change-Id: I193115bc2aa40aa59f1ee4e3ff586ff60967d78e

---
+++ config/overlay.mk
@@ -0,0 +1,19 @@
+# Accent packages
+PRODUCT_PACKAGES += \
+    AmberTheme \
+    CyanTheme \
+    GreenTheme \
+    LimeTheme \
+    OrangeTheme \
+    PinkTheme \
+    RedTheme \
+    YellowTheme \
+
+# QS Theme Packages
+PRODUCT_PACKAGES += \
+    FramedQS \
+    SplitQS \
+    SuperBubbleQS \
+    TeardropQS \
+    HexagonQS \
+    CookieQS \
