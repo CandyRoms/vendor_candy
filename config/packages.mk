f6961e4c7960afaed61f8787c4f3104cab33f781
Anay Wadhera <anay1018@gmail.com>
2019-10-30 12:51:22 -0700

    candy: build font overlays
    Change-Id: I6dbf20c5c02c24e06fdb41020fab6c609cd989aa

--- config/packages.mk
+++ config/packages.mk
@@ -27,6 +27,12 @@ PRODUCT_PACKAGES += \
 # Device Personalization Services
 PRODUCT_PACKAGES += MatchmakerPrebuilt
 
+# Fonts
+PRODUCT_PACKAGES += \
+    FontArbutusSourceOverlay \
+    FontArvoLatoOverlay \
+    FontRubikRubikOverlay \
+
 -include vendor/candy/config/overlay.mk
 
 # Include PixelGApps in build
