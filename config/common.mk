LineageOS/android_vendor_lineage

prebuilt: Add a script to simplify A-only recovery system mount
* This can be helpful because of the fragmentation that will happen with the system mount in recovery after Q

Change-Id: I2d1e775efcf87e33319bc7790d1e54bca72116d3

config/common.mk
@@ -45,6 +45,10 @@ PRODUCT_COPY_FILES += \
     vendor/lineage/prebuilt/common/bin/backuptool_postinstall.sh:$(TARGET_COPY_OUT_SYSTEM)/bin/backuptool_postinstall.sh
 endif
 
+# system mount
+PRODUCT_COPY_FILES += \
+    vendor/lineage/prebuilt/common/bin/system-mount.sh:install/bin/system-mount.sh
+
 # Backup Services whitelist
 PRODUCT_COPY_FILES += \
     vendor/lineage/config/permissions/backup.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/backup.xml

CandyRoms/vendor_candy

config/common.mk
@@ -63,6 +63,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/candy/config/permissions/backup.xml:system/etc/sysconfig/backup.xml
 
+# system mount
+PRODUCT_COPY_FILES += \
+    vendor/candy/prebuilt/common/bin/system-mount.sh:install/bin/system-mount.sh
+
 # Signature compatibility validation
 PRODUCT_COPY_FILES += \
     vendor/candy/prebuilt/common/bin/otasigcheck.sh:install/bin/otasigcheck.sh