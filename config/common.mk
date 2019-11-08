e31b87d288c845a671200136941e35cbc45e39b3
Dan Cartier <nospamdan1@gmail.com>
2019-11-08 09:35:51 -0500

    candy: Permissions consolidation and clean-up
    Change-Id: Ib8593b99feabcb9feae95aae5d0fa796a4e78970

--- config/common.mk
+++ config/common.mk
@@ -100,7 +100,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/handheld_core_hardware.xml \
     vendor/candy/config/permissions/privapp-permissions-candy-system.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-candy.xml \
     vendor/candy/config/permissions/privapp-permissions-candy-product.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-candy.xml \
-    vendor/candy/config/permissions/privapp-permissions-google.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-google.xml \
+    vendor/candy/prebuilt/common/etc/permissions/privapp-permissions-elgoog.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-elgoog.xml \
     vendor/candy/config/permissions/privapp-permissions-google-product.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-google.xml
 
 # Candy-specific startup services
@@ -122,12 +122,6 @@ PRODUCT_COPY_FILES += \
     vendor/candy/prebuilt/common/lib/libjni_latinime.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libjni_latinime.so \
     vendor/candy/prebuilt/common/lib/libjni_latinimegoogle.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libjni_latinimegoogle.so
 endif
-
-# Permissions
-PRODUCT_COPY_FILES += \
-    vendor/candy/config/permissions/candy-privapp-permissions.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/candy-privapp-permissions.xml \
-    vendor/candy/prebuilt/common/etc/permissions/privapp-permissions-candy.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-candy.xml \
-    vendor/candy/prebuilt/common/etc/permissions/privapp-permissions-elgoog.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-elgoog.xml
 
 # Weather client
 #PRODUCT_COPY_FILES += \
