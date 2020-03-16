5122431dbcdbdc9d2319826c502bd5845430f572
Dan Cartier <nospamdan1@gmail.com>
2020-03-16 16:26:00 -0400

    config: Include vendor_addons if present
    * Optional dependency for FOD devices
    
    Change-Id: Iffa79af5fa398dc65467279875c44ce9c9c2cd1b

--- config/common.mk
+++ config/common.mk
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+$(call inherit-product-if-exists, vendor/addons/config.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 
