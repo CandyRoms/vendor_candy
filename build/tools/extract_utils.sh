0495874ca2d7a961729b08a9a71a87b7f752f6cc
dianlujitao <dianlujitao@lineageos.org>
2020-09-12 00:15:13 +0800

    extract_utils: Disable ELF check for shared library
     * This is indeed a nice feature, but it's absolutely a bad idea to
       hardcode dependencies of prebuilt modules in proprietary-files.txt.

--- build/tools/extract_utils.sh
+++ build/tools/extract_utils.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 #
 # Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017-2019 The LineageOS Project
+#               2017-2020 The LineageOS Project
+#               2020 CandyRoms
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -366,6 +367,7 @@ function write_blueprint_packages() {
             if [ "$EXTRA" != "none" ]; then
                 printf '\tcompile_multilib: "%s",\n' "$EXTRA"
             fi
+            printf '\tcheck_elf_files: false,\n'
         elif [ "$CLASS" = "APPS" ]; then
             printf 'android_app_import {\n'
             printf '\tname: "%s",\n' "$PKGNAME"
