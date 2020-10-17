27176aa7419dd583afd8c402f008406206c1422d
Jan Altensen <info@stricted.net>
2020-10-17 18:10:26 +0200

    soong: add TARGET_HAS_MEMFD_BACKPORT conditional
    Change-Id: I71b3a86c96bfe53e93e99c071bc8f3a8c56c77b7

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -34,6 +34,7 @@ SOONG_CONFIG_candyGlobalVars += \
     bootloader_message_offset \
     disable_postrender_cleanup \
     has_legacy_camera_hal1 \
+    has_memfd_backport \
     ignores_ftp_pptp_conntrack_failure \
     needs_netd_direct_connect_rule \
     target_init_vendor_lib \
@@ -63,8 +64,10 @@ SOONG_CONFIG_candyQcomVars += \
 endif
 
 # Soong bool variables
+<<<<<<< HEAD
 SOONG_CONFIG_candyGlobalVars_disable_postrender_cleanup := $(TARGET_DISABLE_POSTRENDER_CLEANUP)
 SOONG_CONFIG_candyGlobalVars_has_legacy_camera_hal1 := $(TARGET_HAS_LEGACY_CAMERA_HAL1)
+SOONG_CONFIG_candyGlobalVars_has_memfd_backport := $(TARGET_HAS_MEMFD_BACKPORT)
 SOONG_CONFIG_candyGlobalVars_ignores_ftp_pptp_conntrack_failure := $(TARGET_IGNORES_FTP_PPTP_CONNTRACK_FAILURE)
 SOONG_CONFIG_candyGlobalVars_needs_netd_direct_connect_rule := $(TARGET_NEEDS_NETD_DIRECT_CONNECT_RULE)
 SOONG_CONFIG_candyNvidiaVars_uses_nv_enhancements := $(NV_ANDROID_FRAMEWORK_ENHANCEMENTS)
