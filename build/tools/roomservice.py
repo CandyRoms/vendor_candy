60699763aa7ad2905b43aeb4c0cc7b75a1e4a1b0
NoSpamDan <nospamdan1@gmail.com>
2020-12-30 18:12:56 -0500

    roomservice: Bump to c11
    Change-Id: Iceb52e99905f46e507c2563fe510e1d9a0f6bc2c

--- build/tools/roomservice.py
+++ build/tools/roomservice.py
@@ -43,7 +43,7 @@ except ImportError:
 DEBUG = False
 default_manifest = ".repo/manifests/candy.xml"
 custom_local_manifest = ".repo/local_manifests/candy_manifest.xml"
-custom_default_revision = "c10"
+custom_default_revision = "c11"
 custom_dependencies = "candy.dependencies"
 org_manifest = "CandyDevices"  # leave empty if org is provided in manifest
 org_display = "CandyDevices"  # needed for displaying
