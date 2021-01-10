19c2dce2467f8618bf8ab6ec385f93adb26fc4a5
NoSpamDan <nospamdan1@gmail.com>
2021-01-10 16:32:00 -0500

    Revert "vendor: Set TARGET_CRYPTFS_HW_PATH if HW crypto is enabled"
    This reverts commit b8555efad71590bb39fc8af2f993043938ce51a5.

--- config/BoardConfigCandy.mk
+++ config/BoardConfigCandy.mk
@@ -1,10 +1,5 @@
 include vendor/candy/config/BoardConfigKernel.mk
 
-# QCOM HW crypto
-ifeq ($(TARGET_HW_DISK_ENCRYPTION),true)
-    TARGET_CRYPTFS_HW_PATH ?= vendor/qcom/opensource/cryptfs_hw
-endif
-
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
 include vendor/candy/config/BoardConfigQcom.mk
 endif
