3a4f26db6aae47f208e99b86b07723ba9f955e20
NoSpamDan <nospamdan1@gmail.com>
2021-01-17 18:59:16 -0500

    repopick: Update to candyroms gerrit
    Change-Id: I3895456b1bd36a7353b62a4a97d79f2485381df9

--- build/tools/repopick.py
+++ build/tools/repopick.py
@@ -142,7 +142,7 @@ def fetch_query(remote_url, query):
 
 if __name__ == '__main__':
     # Default to CANDY Gerrit
-    default_gerrit = 'https://gerrit.bbqdroid.org'
+    default_gerrit = 'https://gerrit.candyroms.org'
 
     parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter, description=textwrap.dedent('''\
         repopick.py is a utility to simplify the process of cherry picking
