3cca0a0416fa9f9d9bc7058c82503d9a24d38bc7
NoSpamDan <nospamdan1@gmail.com>
2021-02-02 23:02:54 -0500

    packages: Don't build duplicate targets when we ship GApps
    * If INCLUDE_GAPPS is used in the build environment, don't build the
      AOSP stock versions.  Affected packages:
        Launcher3
        LivePicker
        LiveWallpapersPicker
    
    Change-Id: I9e078314cb116c1ef18e3b2771b2a73647285a25

--- config/packages.mk
+++ config/packages.mk
@@ -1,14 +1,35 @@
+# Copyright (C) 2021 CandyRoms
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+include_pixelgapps := $(INCLUDE_GAPPS)
+
 # Required packages
 PRODUCT_PACKAGES += \
     bootanimation.zip \
     CandyWrappers \
     Gallery2 \
     LatinIME \
-    LivePicker \
-    Launcher3 \
-    LiveWallpapersPicker \
     messaging \
     Updater
+
+# Packages to add when GApps are omitted from the build
+ifneq ($(include_pixelgapps),true)
+PRODUCT_PACKAGES += \
+    Launcher3 \
+    LivePicker \
+    LiveWallpapersPicker
+endif
 
 # Tools-Candy
 PRODUCT_PACKAGES += \
