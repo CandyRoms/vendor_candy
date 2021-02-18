SuperiorOS/android_vendor_superior

vendor: soong: Add back TARGET_USES_PREBUILT_DYNAMIC_PARTITIONS.
Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>
Change-Id: I35ed626e479f59c212c955a6c61084eedf747f91

config/BoardConfigSoong.mk
@@ -48,6 +48,7 @@ SOONG_CONFIG_superiorGlobalVars += \
     target_ld_shim_libs \
     target_process_sdk_version_override \
     target_surfaceflinger_fod_lib \
+    target_uses_prebuilt_dynamic_partitions \
     uses_camera_parameter_lib
 
 SOONG_CONFIG_NAMESPACES += superiorNvidiaVars
@@ -82,6 +83,7 @@ SOONG_CONFIG_superiorQcomVars_supports_hw_fde_perf := $(TARGET_HW_DISK_ENCRYPTIO
 SOONG_CONFIG_superiorQcomVars_uses_pre_uplink_features_netmgrd := $(TARGET_USES_PRE_UPLINK_FEATURES_NETMGRD)
 SOONG_CONFIG_superiorQcomVars_uses_qcom_bsp_legacy := $(TARGET_USES_QCOM_BSP_LEGACY)
 SOONG_CONFIG_superiorQcomVars_uses_qti_camera_device := $(TARGET_USES_QTI_CAMERA_DEVICE)
+SOONG_CONFIG_superiorGlobalVars_target_uses_prebuilt_dynamic_partitions := $(TARGET_USES_PREBUILT_DYNAMIC_PARTITIONS)
 
 # Set default values
 BOOTLOADER_MESSAGE_OFFSET ?= 0

CandyRoms/vendor_candy

config/BoardConfigSoong.mk
@@ -42,6 +42,7 @@ SOONG_CONFIG_candyGlobalVars += \
     target_ld_shim_libs \
     target_process_sdk_version_override \
     target_surfaceflinger_fod_lib \
+    target_uses_prebuilt_dynamic_partitions \
     uses_camera_parameter_lib
 
 SOONG_CONFIG_NAMESPACES += candyNvidiaVars
@@ -78,6 +79,7 @@ SOONG_CONFIG_candyQcomVars_supports_hw_fde_perf := $(TARGET_HW_DISK_ENCRYPTION_P
 SOONG_CONFIG_candyQcomVars_uses_pre_uplink_features_netmgrd := $(TARGET_USES_PRE_UPLINK_FEATURES_NETMGRD)
 SOONG_CONFIG_candyQcomVars_uses_qcom_bsp_legacy := $(TARGET_USES_QCOM_BSP_LEGACY)
 SOONG_CONFIG_candyQcomVars_uses_qti_camera_device := $(TARGET_USES_QTI_CAMERA_DEVICE)
+SOONG_CONFIG_candyGlobalVars_target_uses_prebuilt_dynamic_partitions := $(TARGET_USES_PREBUILT_DYNAMIC_PARTITIONS)
 
 # Set default values
 BOOTLOADER_MESSAGE_OFFSET ?= 0