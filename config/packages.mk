06f8fc413fbea623ea0f3b719280fc3dab030c3c
jhenrique09 <jhenrique09.mcz@hotmail.com>
2021-04-30 18:07:11 +0000

    vendor: Add back FaceUnlock support
    Change-Id: I92d24bee9db316e8a6e999afe7d2dd3dde598fca

--- config/packages.mk
+++ config/packages.mk
@@ -82,3 +82,14 @@ PRODUCT_PACKAGES += \
 
 # Include Candy theme files
 #include vendor/candy/themes/backgrounds/themes.mk
+
+# Face Unlock
+TARGET_FACE_UNLOCK_SUPPORTED ?= true
+ifeq ($(TARGET_FACE_UNLOCK_SUPPORTED),true)
+PRODUCT_PACKAGES += \
+    FaceUnlockService
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.face_unlock_service.enabled=$(TARGET_FACE_UNLOCK_SUPPORTED)
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
+endif
