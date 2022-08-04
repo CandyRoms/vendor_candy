318284ae922fc42dce8cc6202a64bdc4f64788d8
ShevT <evgeny.shishkov@gmail.com>
2022-08-04 16:17:33 +0300

    soong: Add TARGET_ALTERNATIVE_FUTEX_WAITERS
    Change-Id: I49c7fbf538a5d12ad5df2a4f2be3cd54ded79be2
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -35,6 +35,7 @@ SOONG_CONFIG_candyGlobalVars += \
     camera_needs_miui_camera_mode_support \
     camera_needs_camera_needs_depth_sensor_override \
     gralloc_handle_has_reserved_size \
+    target_alternative_futex_waiters \
     target_health_charging_control_charging_path \
     target_health_charging_control_charging_enabled \
     target_health_charging_control_charging_disabled \
@@ -89,6 +90,7 @@ SOONG_CONFIG_candyGlobalVars_uses_oplus_camera := $(TARGET_USES_OPLUS_CAMERA)
 SOONG_CONFIG_candyGlobalVars_uses_nothing_camera := $(TARGET_USES_NOTHING_CAMERA)
 SOONG_CONFIG_candyGlobalVars_needs_camera_boottime := $(TARGET_CAMERA_BOOTTIME_TIMESTAMP)
 SOONG_CONFIG_candyGlobalVars_camera_needs_client_info_lib := $(TARGET_CAMERA_NEEDS_CLIENT_INFO_LIB)
+SOONG_CONFIG_candyGlobalVars_target_alternative_futex_waiters := $(TARGET_ALTERNATIVE_FUTEX_WAITERS)
 
 # Set default values
 BOOTLOADER_MESSAGE_OFFSET ?= 0
