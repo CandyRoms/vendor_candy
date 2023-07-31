eed064337b9b1e0e109f7e5106a778fe82f9cada
jhenrique09 <jhsv09@gmail.com>
2023-08-01 00:13:25 +0530

    vendor: Allow using legacy fd
    Change-Id: I88e355e60de63a7f81adf8e25fabbd305fb0af7c

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -46,7 +46,8 @@ SOONG_CONFIG_candyGlobalVars += \
     target_trust_usb_control_path \
     target_trust_usb_control_enable \
     target_trust_usb_control_disable \
-    uses_egl_display_array
+    uses_egl_display_array \
+    uses_legacy_fd_fbdev
 
 SOONG_CONFIG_NAMESPACES += candyNvidiaVars
 SOONG_CONFIG_candyNvidiaVars += \
@@ -70,6 +71,7 @@ SOONG_CONFIG_candyGlobalVars_uses_egl_display_array := $(TARGET_USES_EGL_DISPLAY
 SOONG_CONFIG_candyNvidiaVars_uses_nvidia_enhancements := $(NV_ANDROID_FRAMEWORK_ENHANCEMENTS)
 SOONG_CONFIG_candyQcomVars_supports_extended_compress_format := $(AUDIO_FEATURE_ENABLED_EXTENDED_COMPRESS_FORMAT)
 SOONG_CONFIG_candyQcomVars_uses_pre_uplink_features_netmgrd := $(TARGET_USES_PRE_UPLINK_FEATURES_NETMGRD)
+SOONG_CONFIG_candyGlobalVars_uses_legacy_fd_fbdev := $(TARGET_USES_LEGACY_FD_FBDEV)
 
 # Set default values
 BOOTLOADER_MESSAGE_OFFSET ?= 0
