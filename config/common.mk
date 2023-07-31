613527fc7238d40d6fcc4c48df22822eb03710de
afterallafk <shivamatiet2001@gmail.com>
2023-08-01 00:28:41 +0530

    Include packages.mk
    Signed-off-by: afterallafk <shivamatiet2001@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -219,3 +219,6 @@ PRODUCT_PROPERTY_OVERRIDES := \
     ro.config.ringtone=vibe.ogg \
     ro.config.notification_sound=Pikachu.ogg \
     ro.config.alarm_alert=MorningAlarm.ogg
+
+# Packages
+include vendor/blaze/config/packages.mk
