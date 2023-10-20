ProjectBlaze/vendor_blaze

vendor: Add our ringtone and set as default
Signed-off-by: afterallafk <shivamatiet2001@gmail.com>

config/common.mk
@@ -24,6 +24,9 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += ro.adb.secure=1
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += persist.sys.strictmode.disable=true
 endif
 
+# Audio
+$(call inherit-product, vendor/blaze/audio/audio.mk)
+
 # Backup Tool
 PRODUCT_COPY_FILES += \
     vendor/blaze/prebuilt/common/bin/backuptool.sh:install/bin/backuptool.sh \
@@ -213,3 +216,7 @@ PRODUCT_COPY_FILES += \
 endif
 
 include vendor/blaze/config/version.mk
+
+# Sounds (default)
+PRODUCT_PROPERTY_OVERRIDES := \
+    ro.config.ringtone=vibe.ogg

CandyRoms/vendor_candy

config/common.mk
@@ -24,6 +24,9 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += ro.adb.secure=1
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += persist.sys.strictmode.disable=true
 endif
 
+# Audio
+$(call inherit-product, vendor/candy/audio/audio.mk)
+
 # Backup Tool
 PRODUCT_COPY_FILES += \
     vendor/candy/prebuilt/common/bin/backuptool.sh:install/bin/backuptool.sh \
@@ -194,3 +197,8 @@ PRODUCT_EXTRA_RECOVERY_KEYS += \
 include packages/overlays/Themes/themes.mk
 
 include vendor/candy/config/version.mk
+
+# Sounds (default)
+PRODUCT_PROPERTY_OVERRIDES := \
+    ro.config.ringtone=vibe.ogg
+