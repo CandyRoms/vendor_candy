ProjectBlaze/vendor_blaze

soong: Add TARGET_NEEDS_OPLUS_VENDOR_TAG
Change-Id: Ica0675e3b812e32459d82ea3d648829a73f26be5
(cherry picked from commit 11f751a)

config/BoardConfigSoong.mk
@@ -37,7 +37,8 @@ SOONG_CONFIG_blazeGlobalVars += \
     target_inputdispatcher_skip_event_key \
     target_ld_shim_libs \
     target_surfaceflinger_udfps_lib \
-    uses_legacy_fd_fbdev
+    uses_legacy_fd_fbdev \
+    needs_oplus_tag
 
 SOONG_CONFIG_NAMESPACES += blazeNvidiaVars
 SOONG_CONFIG_blazeNvidiaVars += \
@@ -58,6 +59,7 @@ endif
 # Soong bool variables
 SOONG_CONFIG_blazeGlobalVars_needs_camera_boottime := $(TARGET_CAMERA_BOOTTIME_TIMESTAMP)
 SOONG_CONFIG_blazeNvidiaVars_uses_nv_enhancements := $(NV_ANDROID_FRAMEWORK_ENHANCEMENTS)
+SOONG_CONFIG_blazeGlobalVars_needs_oplus_tag := $(TARGET_NEEDS_OPLUS_VENDOR_TAG)
 SOONG_CONFIG_blazeQcomVars_supports_extended_compress_format := $(AUDIO_FEATURE_ENABLED_EXTENDED_COMPRESS_FORMAT)
 SOONG_CONFIG_blazeQcomVars_uses_pre_uplink_features_netmgrd := $(TARGET_USES_PRE_UPLINK_FEATURES_NETMGRD)
 SOONG_CONFIG_blazeQcomVars_uses_qti_camera_device := $(TARGET_USES_QTI_CAMERA_DEVICE)

CandyRoms/vendor_candy

config/BoardConfigSoong.mk
@@ -47,7 +47,8 @@ SOONG_CONFIG_candyGlobalVars += \
     target_trust_usb_control_enable \
     target_trust_usb_control_disable \
     uses_egl_display_array \
-    uses_legacy_fd_fbdev
+    uses_legacy_fd_fbdev \
+    needs_oplus_tag
 
 SOONG_CONFIG_NAMESPACES += candyNvidiaVars
 SOONG_CONFIG_candyNvidiaVars += \
@@ -69,6 +70,7 @@ SOONG_CONFIG_candyGlobalVars_gralloc_handle_has_custom_content_md_reserved_size 
 SOONG_CONFIG_candyGlobalVars_gralloc_handle_has_reserved_size := $(TARGET_GRALLOC_HANDLE_HAS_RESERVED_SIZE)
 SOONG_CONFIG_candyGlobalVars_uses_egl_display_array := $(TARGET_USES_EGL_DISPLAY_ARRAY)
 SOONG_CONFIG_candyNvidiaVars_uses_nvidia_enhancements := $(NV_ANDROID_FRAMEWORK_ENHANCEMENTS)
+SOONG_CONFIG_candyGlobalVars_needs_oplus_tag := $(TARGET_NEEDS_OPLUS_VENDOR_TAG)
 SOONG_CONFIG_candyQcomVars_supports_extended_compress_format := $(AUDIO_FEATURE_ENABLED_EXTENDED_COMPRESS_FORMAT)
 SOONG_CONFIG_candyQcomVars_uses_pre_uplink_features_netmgrd := $(TARGET_USES_PRE_UPLINK_FEATURES_NETMGRD)
 SOONG_CONFIG_candyGlobalVars_uses_legacy_fd_fbdev := $(TARGET_USES_LEGACY_FD_FBDEV)